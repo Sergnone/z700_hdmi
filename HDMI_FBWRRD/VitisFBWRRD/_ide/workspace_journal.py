435d3e0a2a97ad476ecb36b5eda96ebfd62fda8e
Sergey Frolov <sergeyfevg@gmail.com>
2026-02-05 12:25:07 +0300

    Updates src tree

--- HDMI_FBWRRD/VitisFBWRRD/_ide/workspace_journal.py
+++ HDMI_FBWRRD/VitisFBWRRD/_ide/workspace_journal.py
@@ -20,3 +20,10 @@ status = platform.build()
 
 comp.build()
 
+advanced_options = client.create_advanced_options_dict(dt_overlay="0")
+
+platform = client.create_platform_component(name = "platform2",hw_design = "$COMPONENT_LOCATION/../../top_design_wrapper.xsa",os = "linux",cpu = "ps7_cortexa9",domain_name = "linux_ps7_cortexa9",advanced_options = advanced_options)
+
+platform = client.get_component(name="platform2")
+status = platform.build()
+
--- HDMI_VDMA/_ide/workspace_journal_4483.py
+++
@@ -1,8 +0,0 @@
-# 2025-12-22T10:07:28.336583849
-import vitis
-
-client = vitis.create_client()
-client.set_workspace(path="HDMI_TEST_VDMA")
-
-vitis.dispose()
-
